375cb45dbb957ea181ebf0eddde3b5d2c28896ce
Raghav Dhingra <raghav.dhingra15@gmail.com>
2021-07-04 21:19:20 +0530

    added transactions and file checker and creater

--- SoBTC.py
+++ SoBTC.py
@@ -1,4 +1,13 @@
+from os import path
+
 mempool_transactions_array = []
+has_parent_dict = {}
+no_parent_dict = {}
+
+visited_transactions = {}
+non_visited_transactions = {}
+
+max_block_weight = 4000000
 
 class MempoolTransaction(): 
   def __init__(self, txid, fee, weight, parents): 
@@ -17,14 +26,37 @@ class MempoolTransaction():
       return self.txid
 
 
+def check_for_file():
+  file_name = "block.txt"
+  if not path.exists(file_name):
+    open(file_name, "x")
+  else:
+    open(file_name, 'w').close()
+
 def parse_mempool_csv():
   with open('mempool.csv') as f:
     for line in f.readlines():
       transaction_block = line.strip().split(',')
       if transaction_block[0] == "tx_id":
         continue
+      if transaction_block[3] != "":
+        transaction_block[3] = transaction_block[3].split(";")
+        has_parent_dict[transaction_block[0]] = transaction_block
+      else:
+        no_parent_dict[transaction_block[0]] = transaction_block
       mempool_transactions_array.append(MempoolTransaction(*transaction_block))
+  sorted(mempool_transactions_array, reverse=True)
 
-parse_mempool_csv()
-print(mempool_transactions_array)
-print(sorted(mempool_transactions_array, reverse=True))
+def transaction_iterator():
+  for txn in mempool_transactions_array:
+    print(txn.txid)
+    break
+
+
+def initialise():
+  check_for_file()
+  # parse_mempool_csv()
+  # transaction_iterator()
+
+initialise()
+# print(mempool_transactions_array)
